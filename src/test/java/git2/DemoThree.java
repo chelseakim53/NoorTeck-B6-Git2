c2b9c968106b8a188b280901ae67515708de352b
chels <chels@10.0.0.37>
2022-05-05 21:20:53 -0400

    stashing changes

--- src/test/java/git2/DemoThree.java
+++ src/test/java/git2/DemoThree.java
@@ -5,5 +5,11 @@ public class DemoThree {
 	public static void main (String [] args) {
 		System.out.println("Demo Three...");
 	}
+	
+	public void subtract (int a, int b) {
+		int result = a - b;
+		
+		System.out.println(result);
+	}
 
 }
